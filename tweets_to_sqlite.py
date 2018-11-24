32e3af85beb0c20f896329ae4a621b7ce4b5f9c3
Andrea Tarozzo <tarozzo.andrea@gmail.com>
2018-11-24 19:03:35 +0000

    third commit - tweets_to_sqlite has been refined

--- tweets_to_sqlite.py
+++ tweets_to_sqlite.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+
 from tweet_parser import TweetParser
 
 
@@ -10,80 +11,56 @@ class TweetToSqlite:
         self.file_name = file_name
         self.data_to_export = data_to_export
 
-    @staticmethod
-    def __user_table():
-        """
-        :return: The query needed to create the 'users'table.
-        """
-        user_table = "CREATE TABLE users (" \
-                     "user_id INTEGER PRIMARY KEY," \
-                     "user_name text," \
-                     "user_verified int," \
-                     "user_lang text," \
-                     "user_description text," \
-                     "user_followers_number int," \
-                     "user_friends_number int," \
-                     "user_favorites_count int," \
-                     "user_statuses_count int," \
-                     "user_set_location text," \
-                     "location_city text," \
-                     "location_country text," \
-                     "country_code text," \
-                     "geo_latitude int," \
-                     "geo_longitude int," \
-                     "user_account_creation text)"
+        self.tweet_info = ("tweet_id INTEGER PRIMARY KEY", "tweet text", "tweet_length int", "hashtags text",
+                          "mentions text", "is_retweet int", "is_retweeted int", "retweet_count text",
+                          "reply_count int",
+                          "favorite_count int", "lang_tweet text", "source text", "created text", "created_date text",
+                          "created_year int", "created_month int", "created_day int", "created_hour int",
+                          "created_minute int", "user_name text")
 
-        return user_table
+        self.user_info = ("user_name text PRIMARY KEY", "user_verified int", "user_lang text", "user_description text",
+                           "user_followers_number int", "user_friends_number int", "user_favorites_count int",
+                           "user_statuses_count int", "user_set_location text", "location_city text",
+                           "location_country text",
+                           "country_code text", "geo_latitude int", "geo_longitude int", "user_account_creation text")
 
     @staticmethod
-    def __tweet_table():
+    def __create_table(table_info, table_name):
         """
-        :return: The query needed to create the 'tweets'table.
+        This functions creates the query needed to the table creation
         """
-        tweet_table = "CREATE TABLE tweets (" \
-                      "tweet_id INTEGER PRIMARY KEY," \
-                      "tweet text," \
-                      "tweet_length int," \
-                      "hashtags text," \
-                      "mentions text," \
-                      "is_retweet int," \
-                      "is_retweeted int," \
-                      "retweet_count text," \
-                      "reply_count int," \
-                      "favorite_count int," \
-                      "lang_tweet text," \
-                      "source text," \
-                      "created text," \
-                      "created_date text," \
-                      "created_year int," \
-                      "created_month int," \
-                      "created_day int," \
-                      "created_hour int," \
-                      "created_minute int," \
-                      "user_name text)"
+        query = "CREATE TABLE " + table_name + " ("
+        for x in range(len(table_info)):
+            if table_info[x] == table_info[-1]:
+                query += table_info[x] + ")"
+            else:
+                query += table_info[x] + ","
 
-        return tweet_table
+        return query
 
     def __create_db(self):
         """
         This function create the db, the value of variable 'data_to_export' states which table/tables
         must be created.
         """
-        user_table = self.__user_table()
-        tweet_table = self.__tweet_table()
-
-        print(self.file_name)
         connection = sqlite3.connect(self.file_name + ".db")
         cursor = connection.cursor()
 
         if self.data_to_export == "full_tweet":
+            user_table = self.__create_table(self.user_info, "users")
+            tweet_table = self.__create_table(self.tweet_info, "tweets")
+            tweet_table = tweet_table.replace("user_name text",
+                                              "user_name text, FOREIGN KEY (user_name) REFERENCES users(user_name)")
+            print(tweet_table)
             cursor.execute(user_table)
             cursor.execute(tweet_table)
 
         elif self.data_to_export == "user_only":
+            user_table = self.__create_table(self.user_info, "users")
             cursor.execute(user_table)
 
         elif self.data_to_export == "tweet_only":
+            tweet_table = self.__create_table(self.tweet_info, "tweets")
             cursor.execute(tweet_table)
 
         else:
@@ -100,7 +77,7 @@ class TweetToSqlite:
         user_values = (self.parser.user_data_only(tweet, output_type="tuple"))
         if len(user_values) == 0:
             return None
-        user_query = "INSERT INTO users VALUES (NULL"+",?"*len(user_values)+")"
+        user_query = "INSERT INTO users VALUES (?" + ",?" * (len(user_values)-1) + ")"
         return user_values, user_query
 
     def __tweet_values(self, tweet):
@@ -111,7 +88,7 @@ class TweetToSqlite:
         tweet_values = (self.parser.tweet_data_only(tweet, output_type="tuple"))
         if len(tweet_values) == 0:
             return None
-        tweet_query = "INSERT INTO tweets VALUES (NULL"+",?"*len(tweet_values)+")"
+        tweet_query = "INSERT INTO tweets VALUES (NULL" + ",?" * len(tweet_values) + ")"
         return tweet_values, tweet_query
 
     def __commit_values(self, tweet):
@@ -156,3 +133,5 @@ class TweetToSqlite:
             self.__commit_values(tweet)
         else:
             self.__commit_values(tweet)
+
+
